4304740b1d7ef20ac7e436912e3f0d6548993e48
Doug Jones <dsjr1284@gmail.com>
2020-04-13 23:00:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,22 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    def execute_transaction
-    if @sender.balance > @amount && ACCOUNTS ARE VALID && STATUS IS ...?
-       @sender.balance -= @amount
-       @receiver.balance += @amount
-       @status = "complete"
-    else
-      @status = "rejected"
-      return  "Transaction rejected. Please check your account balance."
-    end
-  end
-
-
-
-
-
-    if @sender.balance > @amount and @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
