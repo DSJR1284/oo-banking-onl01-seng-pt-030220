3e0cce9bf70dd5ea8fac400403cd9299778eae12
Doug Jones <dsjr1284@gmail.com>
2020-04-13 22:18:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else 
-      rejected 
+    else
+      rejected
 
     end
   end
