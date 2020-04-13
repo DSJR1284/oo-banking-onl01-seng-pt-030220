78ea8a44bec1e399c7b369f1073c6ff6a5ce2e1e
Doug Jones <dsjr1284@gmail.com>
2020-04-13 22:32:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount and @status
+    if @sender.balance > @amount and @status 
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
@@ -28,5 +28,7 @@ class Transfer
       return  "Transaction rejected. Please check your account balance."
     end
   end
+
+  
 
 end
